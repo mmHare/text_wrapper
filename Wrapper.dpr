3cc670133ef2e7e5e08376c47ac4a8edaf1a231c
mmHare <madmarchhare65@gmail.com>
2025-08-29 15:10:41 +0200

    Project creation

---
+++ Wrapper.dpr
@@ -0,0 +1,14 @@
+program Wrapper;
+
+uses
+  Vcl.Forms,
+  frmMain in 'frmMain.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
